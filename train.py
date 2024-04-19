ac6b33b8e75c43d31bc0d45a88a5c094411f6530
Michael Scherbela <michael.scherbela@gmail.com>
2024-04-19 17:36:11 +0200

    Undo accidental revert of edits in train.py

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import logging
+import os
 
-# chex.fake_pmap_and_jit().start()
-import pathlib
 import jax
 import jax.numpy as jnp
 import jax.tree_util as jtu
@@ -10,25 +9,23 @@ import optax
 import pyscf
 import tqdm
 from seml.experiment import Experiment
-from sparse_wf.api import AuxData, ModelArgs, LoggingArgs, OptimizationArgs
-from sparse_wf.mcmc import make_mcmc, make_width_scheduler, init_electrons
+from sparse_wf.api import AuxData, LoggingArgs, ModelArgs, OptimizationArgs
+from sparse_wf.jax_utils import assert_identical_copies
 from sparse_wf.loggers import MultiLogger
-
-from sparse_wf.model.moon import SparseMoonWavefunction  # noqa: F401
+from sparse_wf.mcmc import make_mcmc, make_width_scheduler, init_electrons
 from sparse_wf.model.dense_ferminet import DenseFermiNet  # noqa: F401
+from sparse_wf.model.moon import SparseMoonWavefunction  # noqa: F401
 from sparse_wf.preconditioner import make_preconditioner
 from sparse_wf.pretraining import make_pretrainer
 from sparse_wf.systems.scf import make_hf_orbitals
 from sparse_wf.update import make_trainer
-from sparse_wf.jax_utils import broadcast, p_split
-
 
 jax.config.update("jax_default_matmul_precision", "float32")
 jax.config.update("jax_enable_x64", False)
 ex = Experiment()
 
 
-ex.add_config(str(pathlib.Path(__file__).parent / "config/default.yaml"))
+ex.add_config("config/default.yaml")
 
 
 def to_log_data(aux_data: AuxData) -> dict[str, float]:
@@ -54,20 +51,36 @@ def main(
     logging_args: LoggingArgs,
 ):
     config = locals()
-    # TODO : add entity and make project configurable
     loggers = MultiLogger(logging_args)
     loggers.log_config(config)
-    key = jax.random.PRNGKey(seed)
+    # initialize distributed training
+    if int(os.environ.get("SLURM_NTASKS", 1)) > 1:
+        jax.distributed.initialize()
+    logging.info(f"Using {jax.device_count()} devices across {jax.process_count()} processes.")
 
     mol = pyscf.gto.M(atom=molecule, basis=basis, spin=spin, unit="bohr")
     mol.build()
 
     # wf = SparseMoonWavefunction.create(mol, **model_args)
     wf = DenseFermiNet.create(mol)
-    key, subkey = jax.random.split(key)
+
+    # Setup random keys
+    # the main key will always be identitcal on all processes
+    main_key = jax.random.PRNGKey(seed)
+    # the proc_key will be unique per process.
+    main_key, subkey = jax.random.split(main_key)
+    proc_key = jax.random.split(subkey, jax.process_count())[jax.process_index()]
+    # device_keys will be unique per device.
+    proc_key, subkey = jax.random.split(proc_key)
+    device_keys = jax.random.split(subkey, jax.local_device_count())
+
+    # We want the parameters to be identical so we use the main_key here
+    main_key, subkey = jax.random.split(main_key)
     params = wf.init(subkey)
-    logging.info(f"Number of parameters: {sum(jnp.size(p) for p in jax.tree_leaves(params))}")
-    key, subkey = jax.random.split(key)
+    logging.info(f"Number of parameters: {sum(jnp.size(p) for p in jtu.tree_leaves(params))}")
+
+    # We want to initialize differently per process so we use the proc_key here
+    proc_key, subkey = jax.random.split(proc_key)
     electrons = init_electrons(subkey, mol, batch_size)
 
     trainer = make_trainer(
@@ -81,18 +94,16 @@ def main(
         make_preconditioner(wf, optimization["preconditioner_args"]),
         optimization["clipping"],
     )
-    state = trainer.init(key, params, electrons, jnp.array(init_width))
+    # The state will only be fed into pmapped functions, i.e., we need a per device key
+    state = trainer.init(device_keys, params, electrons, jnp.array(init_width))
+    assert_identical_copies(state.params)
 
     pretrainer = make_pretrainer(trainer, make_hf_orbitals(mol, basis), optax.adam(1e-3))
     state = pretrainer.init(state)
 
-    key, *subkeys = jax.random.split(key, jax.device_count() + 1)
-    shared_key = broadcast(jnp.stack(subkeys))
-
     logging.info("Pretraining")
     with tqdm.trange(pretrain_steps) as pbar:
         for _ in pbar:
-            shared_key, subkey = p_split(shared_key)
             static = wf.input_constructor.get_static_input(state.electrons)
             state, aux_data = pretrainer.step(state, static)
             aux_data = to_log_data(aux_data)
@@ -102,11 +113,11 @@ def main(
             set_postfix(pbar, aux_data)
 
     state = state.to_train_state()
+    assert_identical_copies(state.params)
 
     logging.info("Training")
     with tqdm.trange(optimization["steps"]) as pbar:
         for opt_step in pbar:
-            shared_key, subkey = p_split(shared_key)
             static = wf.input_constructor.get_static_input(state.electrons)
             state, _, aux_data = trainer.step(state, static)
             aux_data = to_log_data(aux_data)
@@ -114,3 +125,4 @@ def main(
             if np.isnan(aux_data["opt/E"]):
                 raise ValueError("NaN in energy")
             set_postfix(pbar, aux_data)
+    assert_identical_copies(state.params)
