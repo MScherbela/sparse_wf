3d39e69805052254dd7c48b5b800c68006f0b0d9
Nicholas Gao <nicholas.gao@tum.de>
2024-04-22 14:39:33 +0200

    use new optimizer configuration for pretraining

--- train.py
+++ train.py
@@ -5,11 +5,10 @@ import jax
 import jax.numpy as jnp
 import jax.tree_util as jtu
 import numpy as np
-import optax
 import pyscf
 import tqdm
 from seml.experiment import Experiment
-from sparse_wf.api import AuxData, Electrons, LoggingArgs, ModelArgs, OptimizationArgs, PRNGKeyArray
+from sparse_wf.api import AuxData, Electrons, LoggingArgs, ModelArgs, OptimizationArgs, PRNGKeyArray, PretrainingArgs
 from sparse_wf.jax_utils import assert_identical_copies
 from sparse_wf.loggers import MultiLogger
 from sparse_wf.mcmc import make_mcmc, make_width_scheduler
@@ -51,8 +50,8 @@ def main(
     spin: int,
     model_args: ModelArgs,
     optimization: OptimizationArgs,
+    pretraining: PretrainingArgs,
     batch_size: int,
-    pretrain_steps: int,
     mcmc_steps: int,
     init_width: float,
     basis: str,
@@ -106,11 +105,11 @@ def main(
     state = trainer.init(device_keys, params, electrons, jnp.array(init_width))
     assert_identical_copies(state.params)
 
-    pretrainer = make_pretrainer(trainer, make_hf_orbitals(mol, basis), optax.adam(1e-3))
+    pretrainer = make_pretrainer(trainer, make_hf_orbitals(mol, basis), make_optimizer(**pretraining["optimizer_args"]))
     state = pretrainer.init(state)
 
     logging.info("Pretraining")
-    with tqdm.trange(pretrain_steps) as pbar:
+    with tqdm.trange(pretraining["steps"]) as pbar:
         for _ in pbar:
             static = wf.input_constructor.get_static_input(state.electrons)
             state, aux_data = pretrainer.step(state, static)
