e5ad3068f81760de05f16c40dd67a7d4b71472c1
garctaedius <gunnar@arctaedius.com>
2024-05-07 12:11:16 +0200

    Added a configuration for running on vsc5

---
+++ config/slurm_templates/vsc5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J {job_name}
+#SBATCH -N {n_nodes}
+#SBATCH --partition {partition}
+#SBATCH --qos {qos}
+#SBATCH --output stdout.txt
+#SBATCH --time {time}
+#SBATCH --gres=gpu:{n_gpus}
+#SBATCH --ntasks-per-node {n_gpus}
+
+module purge
+source /gpfs/opt/sw/spack-0.17.1/opt/spack/linux-almalinux8-zen3/gcc-11.2.0/miniconda3-4.12.0-ap65vga66z2rvfcfmbqopba6y543nnws/etc/profile.d/conda.sh
+conda activate sparse_wf
+export OMP_NUM_THREADS=10
+export MKL_NUM_THREADS=10
+export NVIDIA_TF32_OVERRIDE=0
+export WANDB_DIR="${{HOME}}/tmp"
+srun sparse-wf-run full_config.yaml
