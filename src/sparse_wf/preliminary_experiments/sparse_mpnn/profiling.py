846b921c235dbdc848f3664c217b2bd0dd0a6de4
Michael Scherbela <michael.scherbela@gmail.com>
2024-07-04 11:02:53 +0200

    update profiling script

--- src/sparse_wf/preliminary_experiments/sparse_mpnn/profiling.py
+++ src/sparse_wf/preliminary_experiments/sparse_mpnn/profiling.py
@@ -2,7 +2,8 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 os.environ["NVIDIA_TF32_OVERRIDE"] = "0"
 from sparse_wf.model.wave_function import MoonLikeWaveFunction
-from sparse_wf.api import JastrowArgs, EmbeddingArgs
+from sparse_wf.api import JastrowArgs, EmbeddingArgs, EnvelopeArgs
+from sparse_wf.mcmc import make_mcmc
 import jax
 import jax.numpy as jnp
 from pyscf.gto import Mole
@@ -25,42 +26,50 @@ def build_atom_chain(rng, n_nuc, n_el_per_nuc, batch_size):
     mol = Mole(atom=[(int(Z_), R_) for R_, Z_ in zip(R, Z)]).build()
     return r, mol
 
-batch_size = 64
+batch_size = 8
 n_determinants = 16
-rng_r, rng_model = jax.random.split(jax.random.PRNGKey(0))
-electrons, mol = build_atom_chain(rng_r, n_nuc=180, n_el_per_nuc=1, batch_size=batch_size)
+mcmc_intersteps = 50
+rng_r, rng_model, rng_mcmc = jax.random.split(jax.random.PRNGKey(0), 3)
+electrons, mol = build_atom_chain(rng_r, n_nuc=362, n_el_per_nuc=1, batch_size=batch_size)
 n_el = electrons.shape[-2]
 
 model = MoonLikeWaveFunction.create(
     mol,
     EmbeddingArgs(
-        cutoff=5.0, feature_dim=256, nuc_mlp_depth=4, pair_mlp_widths=(16, 8), pair_n_envelopes=16
+        cutoff=5.0, cutoff_1el=20.0, feature_dim=256, nuc_mlp_depth=4, pair_mlp_widths=(16, 8), pair_n_envelopes=16, low_rank_buffer=2,
     ),
     JastrowArgs(e_e_cusps="psiformer", use_log_jastrow=True, use_mlp_jastrow=True, mlp_depth=2, mlp_width=64),
+    EnvelopeArgs(envelope="isotropic", isotropic_args=dict(n_envelopes=8), glu_args=None), # type: ignore
     n_determinants,
-    n_envelopes=8,
+
 )
 params = model.init(rng_model, electrons[0])
 params = jtu.tree_map(lambda x: jnp.array(x, dtype), params)
 static_args = model.get_static_input(electrons)
 print(static_args)
 
-# get_E_loc = jax.jit(jax.vmap(model.local_energy, in_axes=(None, 0, None)), static_argnums=2)
+get_E_loc = jax.jit(jax.vmap(model.local_energy, in_axes=(None, 0, None)), static_argnums=2)
 get_logpsi = jax.jit(jax.vmap(model, in_axes=(None, 0, None)), static_argnums=2)
+mcmc_step, stepsize = make_mcmc(model, "single-electron", 0.2, mcmc_intersteps)
+mcmc_step = jax.jit(mcmc_step, static_argnums=3)
+
+def func_to_profile(params, electrons, static_args):
+    # result = get_E_loc(params, electrons, static_args)
+    result = mcmc_step(rng_mcmc, params, electrons, static_args, stepsize)[0]
+    return result.block_until_ready()
 
 print("Running warmup passes for compilation")
-for i in range(5):
+for i in range(3):
     print(f"Warmup pass {i}")
-    Eloc = get_logpsi(params, electrons, static_args).block_until_ready()
+    result = func_to_profile(params, electrons, static_args)
 
 print("Running actual passes for profiling")
 with jax.profiler.trace("/tmp/tensorboard"):
     timings = []
-    for i in range(5):
+    for i in range(3):
         print(f"Pass {i}")
         t0 = time.perf_counter()
-        # Eloc = get_E_loc(params, electrons, static_args).block_until_ready()
-        logpsi = get_logpsi(params, electrons, static_args).block_until_ready()
+        result = func_to_profile(params, electrons, static_args)
         t1 = time.perf_counter()
         timings.append(t1 - t0)
     print(f"Duration: T={np.mean(timings):.3f} +- {np.std(timings) / np.sqrt(len(timings)):.3f} s")
