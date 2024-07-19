b8435fd5ef63e6fc2c9ea6066c5e3131a673fbdb
Michael Scherbela <michael.scherbela@gmail.com>
2024-07-19 22:33:10 +0200

    Added MLP with sparse_lap

--- src/sparse_wf/preliminary_experiments/sparse_indexing/sparse_model.py
+++ src/sparse_wf/preliminary_experiments/sparse_indexing/sparse_model.py
@@ -1,5 +1,6 @@
 # %%
 import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["NVIDIA_TF32_OVERRIDE"] = "0"
 import jax.numpy as jnp
@@ -15,6 +16,100 @@ from sparse_wf.jax_utils import fwd_lap
 import functools
 from folx.api import FwdLaplArray
 import jax.tree_util as jtu
+
+
+class FwdLapTuple(NamedTuple):
+    x: jax.Array
+    jac: jax.Array
+    lap: jax.Array
+    idx_ctr: jax.Array
+    idx_dep: jax.Array
+
+    def get_jac_sqr(self):
+        jac_sqr = jnp.zeros_like(self.x)
+        jac_sqr = jac_sqr.at[self.idx_ctr].add(jnp.sum(self.jac**2, axis=1), mode="drop")
+        return jac_sqr
+
+    def dense_jac(self):
+        n_el = np.max(self.idx_ctr) + 1
+        feature_dim = self.jac.shape[-1]
+        J_dense = np.zeros((n_el, 3, n_el, feature_dim))  # [dep, xyz, i, feature_dim]
+        J_dense[self.idx_dep, :, self.idx_ctr, :] = self.jac
+        J_dense = J_dense.reshape([n_el * 3, n_el, feature_dim])
+        return J_dense
+
+
+class Linear(PyTreeNode):
+    features: int
+    use_bias: bool = True
+
+    def apply(self, params, x):
+        y = x @ params["kernel"]
+        if self.use_bias:
+            y += params["bias"]
+        return y
+
+    def apply_with_fwd_lap(self, params, x: FwdLapTuple):
+        y = x.x @ params["kernel"]
+        if self.use_bias:
+            y += params["bias"]
+        jac = x.jac @ params["kernel"]
+        lap = x.lap @ params["kernel"]
+        return FwdLapTuple(y, jac, lap, x.idx_ctr, x.idx_dep)
+
+    def init(self, rng, x):
+        return dict(
+            kernel=jax.nn.initializers.lecun_normal(dtype=jnp.float32)(rng, (x.shape[-1], self.features)),
+            bias=jnp.zeros((self.features,), dtype=jnp.float32),
+        )
+
+
+class ElementWise(PyTreeNode):
+    fn: callable
+
+    def apply(self, x):
+        return self.fn(x)
+
+    def apply_with_fwd_lap(self, x: FwdLapTuple):
+        grad_fn = jax.grad(self.fn)
+        hess_fn = jax.grad(grad_fn)
+        y = [jnp.vectorize(f, signature="()->()")(x.x) for f in [self.fn, grad_fn, hess_fn]]
+        return FwdLapTuple(
+            y[0],
+            x.jac * y[1].at[x.idx_ctr, None].get(mode="fill", fill_value=0.0),
+            x.lap * y[1] + x.get_jac_sqr() * y[2],
+            x.idx_ctr,
+            x.idx_dep,
+        )
+
+
+class MLP(PyTreeNode):
+    width: int
+    depth: int
+    activate_final: bool = False
+    activation: callable = nn.silu
+
+    def apply(self, params, x):
+        for i, p in enumerate(params):
+            x = Linear(self.width).apply(p, x)
+            if i < len(params) - 1 or self.activate_final:
+                x = ElementWise(self.activation).apply(x)
+        return x
+
+    def apply_with_fwd_lap(self, params, x: FwdLapTuple):
+        for i, p in enumerate(params):
+            x = Linear(self.width).apply_with_fwd_lap(p, x)
+            if i < len(params) - 1 or self.activate_final:
+                x = ElementWise(self.activation).apply_with_fwd_lap(x)
+        return x
+
+    def init(self, rng, x):
+        params = []
+        for _ in range(self.depth):
+            rng, key = jax.random.split(rng)
+            params.append(Linear(self.width).init(key, x))
+            x = np.zeros_like(x, shape=(self.width,))
+        return params
 
 
 class ElecInit(nn.Module):
@@ -87,8 +182,10 @@ def contract(Gamma, h, idx_ctr, idx_nb):
     h_out = h_out.at[idx_ctr, :].add(Gamma * h[idx_nb, :], mode="drop")
     return h_out
 
+
 def get_with_fill(x, idx):
     return x.at[idx].get(mode="fill", fill_value=0.0)
+
 
 # TODO: all indexing with .at[].get(mode="fill", fill_value=0.0)
 def contract_with_fwd_lap(Gamma, h, idx_ctr, idx_nb):
@@ -109,15 +206,17 @@ def contract_with_fwd_lap(Gamma, h, idx_ctr, idx_nb):
 
     return (h_out, J_out)
 
+
 def build_dense_jacobian(J, indices):
     n_el = np.max(indices) + 1
     feature_dim = J.shape[-1]
-    J_dense = np.zeros((n_el, 3, n_el, feature_dim)) # [dep, xyz, i, feature_dim]
-    i,j = indices
+    J_dense = np.zeros((n_el, 3, n_el, feature_dim))  # [dep, xyz, i, feature_dim]
+    i, j = indices
     J_dense[np.arange(n_el), :, np.arange(n_el), :] = J[:n_el, :, :]
     J_dense[j, :, i, :] = J[n_el:, :, :]
     J_dense = J_dense.reshape([n_el * 3, n_el, feature_dim])
     return J_dense
+
 
 class Embedding(PyTreeNode):
     # Hyperparams
@@ -128,7 +227,7 @@ class Embedding(PyTreeNode):
     # Submodules
     elec_init: ElecInit
     edge: PairWiseFunction
-    updates: list[ElectronUpdate]
+    updates: list[MLP]
 
     @classmethod
     def create(cls, feature_dim, n_layers, cutoff):
@@ -138,7 +237,7 @@ class Embedding(PyTreeNode):
             cutoff,
             elec_init=ElecInit(feature_dim, n_layers + 1),
             edge=PairWiseFunction(cutoff, feature_dim, n_layers),
-            updates=[ElectronUpdate() for _ in range(n_layers)],
+            updates=[MLP(feature_dim, 2) for _ in range(n_layers)],
         )
 
     def init(self, rng):
@@ -146,10 +245,7 @@ class Embedding(PyTreeNode):
         return EmbeddingParams(
             self.elec_init.init(rngs[0], np.zeros(3)),
             self.edge.init(rngs[1], np.zeros(3), np.zeros(3)),
-            [
-                u.init(key, np.zeros(self.feature_dim), np.zeros(self.feature_dim))
-                for u, key in zip(self.updates, rngs[2:])
-            ],
+            [u.init(key, np.zeros(self.feature_dim)) for u, key in zip(self.updates, rngs[2:])],
         )
 
     def get_static_args(self, r) -> StaticArgs[Int]:
@@ -182,12 +278,13 @@ class Embedding(PyTreeNode):
         Gamma = jax.vmap(lambda i, j: self.edge.apply(params.edge, r[i], r[j]))(idx_ct, idx_nb)
 
         msg = contract(Gamma[0], h0[1], idx_ct, idx_nb)
+        h_out = self.updates[0].apply(params.updates[0], msg)
         # h = h0[0]
         # for l, (h_nb, g) in enumerate(zip(h0[1:], Gamma)):
         #     msg = contract(g, h_nb, idx_ct, idx_nb)
-            # h = self.updates[l].apply(params.updates[l], h, msg)
+        # h = self.updates[l].apply(params.updates[l], h, msg)
 
-        return msg
+        return h_out
 
     def apply_with_fwd_lap(self, params, r, static: StaticArgs[int]):
         n_el = r.shape[0]
@@ -198,12 +295,21 @@ class Embedding(PyTreeNode):
 
         h0 = jax.vmap(fwd_lap(h0_fn))(r)
         Gamma = jax.vmap(fwd_lap(edge_fn))(r[idx_ct], r[idx_nb])
-        msg = contract_with_fwd_lap(Gamma[0], h0[1], idx_ct, idx_nb)
+        msg, msg_jac = contract_with_fwd_lap(Gamma[0], h0[1], idx_ct, idx_nb)
+        msg = FwdLapTuple(
+            msg,
+            msg_jac,
+            jnp.zeros_like(msg),
+            jnp.concatenate([jnp.arange(n_el), idx_ct]),
+            jnp.concatenate([jnp.arange(n_el), idx_nb]),
+        )
+        h_out = self.updates[0].apply_with_fwd_lap(params.updates[0], msg)
         # h = h0[0]
         # for l, (h_nb, g) in enumerate(zip(h0[1:], Gamma)):
         #     msg = contract_with_fwd_lap(g, h_nb, idx_ct, idx_nb)
 
-        return msg, (idx_ct, idx_nb)
+        return h_out
+
 
 def is_close(a, b, msg=""):
     try:
@@ -221,17 +327,14 @@ if __name__ == "__main__":
     static = emb.get_static_args(r).to_static()
     print(static)
 
-    h = emb.apply(params, r, static)
     h_folx = fwd_lap(emb.apply, argnums=1)(params, r, static)
+    h_sparse = emb.apply_with_fwd_lap(params, r, static)
+
     # h_dense = emb.apply_dense(params, r, static)
     # h = emb.apply(params, r, static)
     # is_close(h_dense, h, "h_dense != h")
     # is_close(h_folx.x, h, "h_folx.x != h")
 
-
-    (h_sparse, J_sparse), pair_indices = emb.apply_with_fwd_lap(params, r, static)
-    J_dense = build_dense_jacobian(J_sparse, pair_indices)
-
-    is_close(h_folx.x, h_sparse, "h_folx.x != h_sparse")
-    is_close(h_folx.jacobian.data, J_dense, "J_folx != J_sparse")
+    is_close(h_folx.x, h_sparse.x, "h_folx.x != h_sparse.x")
+    is_close(h_folx.jacobian.data, h_sparse.dense_jac(), "h_folx.jac != h_sparse.jac")
     # # h = emb.apply(params, r, static)
