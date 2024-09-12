18e1c6561889227383cf51380047ac26fc933ada
Michael Scherbela <michael.scherbela@gmail.com>
2024-09-12 11:16:39 +0200

    Fix slurm templates for hgx and vsc

--- config/slurm_templates/hgx.sh
+++ config/slurm_templates/hgx.sh
@@ -9,13 +9,14 @@
 #SBATCH --time {time}
 #SBATCH --gres=gpu:{n_gpus}
 #SBATCH --mem={n_gpus*100_000}
-#SBATCH --signal=USR1@300
+#SBATCH --signal=B:USR1@300
 #SBATCH --export=NONE
 SLURM_EXPORT_ENV=ALL
 # The export=None ensures that no environment variables from submission are inherited by the job
 # The SLURM_EXPORT_ENV=ALL ensures that all environment variables present in this file
 # (and the ones set by SLURM) are exported to the srun tasks
 
+trap 'touch SPARSEWF_ABORT && wait' SIGUSR1
 
 source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate sparse_wf
@@ -23,4 +24,5 @@ export OMP_NUM_THREADS=10
 export MKL_NUM_THREADS=10
 export NVIDIA_TF32_OVERRIDE=0
 export WANDB_DIR="${{HOME}}/tmp"
-srun sparse-wf-run full_config.yaml
+srun sparse-wf-run full_config.yaml &
+wait
--- config/slurm_templates/vsc5.sh
+++ config/slurm_templates/vsc5.sh
@@ -7,13 +7,14 @@
 #SBATCH --time {time}
 #SBATCH --gres=gpu:{min(n_gpus, 2)}
 #SBATCH --ntasks-per-node={min(n_gpus, 2)}
-#SBATCH --signal=USR1@300
+#SBATCH --signal=B:USR1@300
 #SBATCH --export=NONE
 SLURM_EXPORT_ENV=ALL
 # The export=None ensures that no environment variables from submission are inherited by the job
 # The SLURM_EXPORT_ENV=ALL ensures that all environment variables present in this file
 # (and the ones set by SLURM) are exported to the srun tasks
 
+trap 'touch SPARSEWF_ABORT && wait' SIGUSR1
 
 module purge
 source /gpfs/opt/sw/spack-0.17.1/opt/spack/linux-almalinux8-zen3/gcc-11.2.0/miniconda3-4.12.0-ap65vga66z2rvfcfmbqopba6y543nnws/etc/profile.d/conda.sh
@@ -22,4 +23,5 @@ export OMP_NUM_THREADS=10
 export MKL_NUM_THREADS=10
 export NVIDIA_TF32_OVERRIDE=0
 export WANDB_DIR="${{HOME}}/tmp"
-srun sparse-wf-run full_config.yaml
+srun sparse-wf-run full_config.yaml &
+wait
