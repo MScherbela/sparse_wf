138ee89a8cea7dc35e15b4764dc2d0b844738dc1
Michael Scherbela <michael.scherbela@gmail.com>
2024-10-15 16:32:09 +0200

    Fix active space selection for very small basis sets (e.g. N2 in STO-6G)

--- src/sparse_wf/scf.py
+++ src/sparse_wf/scf.py
@@ -171,7 +171,8 @@ class CASWavefunction(HFWavefunction):
         self.ci_coeffs = jnp.zeros([n_determinants], dtype=jnp.float32)
 
         with only_on_main_process():
-            cas_result = run_cas(self.hf, min(active_orbitals, mol.nao), min(active_electrons, sum(mol.nelec)), s2)
+            active_orbitals, active_electrons = self.sanity_check_active_space(active_orbitals, active_electrons)
+            cas_result = run_cas(self.hf, active_orbitals, active_electrons, s2)
             self.cas_result = cas_result
             logging.info(f"CASCI energy: {cas_result.energy}")
             idx_orbitals, ci_coeffs = get_most_important_determinants(cas_result, n_determinants, det_threshold)
@@ -184,6 +185,13 @@ class CASWavefunction(HFWavefunction):
             self.ci_coeffs = jnp.asarray(self.ci_coeffs, dtype=jnp.float32)
         self.idx_orbitals = copy_from_main(replicate(self.idx_orbitals))[0]
         self.ci_coeffs = copy_from_main(replicate(self.ci_coeffs))[0]
+
+    def sanity_check_active_space(self, active_orb, active_el):
+        n_el = sum(self.mol.nelec)
+        active_el = min(active_el, n_el)
+        n_core = n_el - active_el
+        active_orb = min(active_orb, self.mol.nao - n_core)
+        return active_orb, active_el
 
     def orbitals(self, electrons: Electrons):
         mo_up, mo_dn = eval_molecular_orbitals(self.mol, self.mo_coeffs, electrons)
