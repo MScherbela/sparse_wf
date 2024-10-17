08df799728f654fdc23b4c1fba0a604913f2074e
Michael Scherbela <michael.scherbela@gmail.com>
2024-10-17 10:24:24 +0200

    Add logic to reduce static padding factor

--- src/sparse_wf/static_args.py
+++ src/sparse_wf/static_args.py
@@ -16,23 +16,44 @@ def round_with_padding(n, padding_factor, max_val):
 
 class StaticScheduler:
     def __init__(
-        self, n_electrons: int, n_up: int, n_nuclei: int, history_length: int = 20, padding_factor: float = 1.2
+        self,
+        n_electrons: int,
+        n_up: int,
+        n_nuclei: int,
+        history_length: int = 100,
+        max_padding_factor: float = 1.2,
+        min_padding_factor: float = 1.05,
     ):
         self.step = 0
+        self.steps_since_recompile = 0
         self.history_length = history_length
         self.history: Optional[StaticInput] = None
+        self.previous_static: Optional[StaticInput] = None
         self.n_electrons = n_electrons
         self.n_up = n_up
         self.n_nuclei = n_nuclei
-        self.padding_factor = padding_factor
+        self.padding_factor = max_padding_factor
+        self.max_padding_factor = max_padding_factor
+        self.min_padding_factor = min_padding_factor
 
     def __call__(self, actual_static: StaticInput) -> StaticInput:
         if self.history is None:
             self.history = tree_zeros_like(actual_static, jnp.int32, self.history_length)
+            self.previous_static = tree_zeros_like(actual_static, jnp.int32)
         self.history = jtu.tree_map(
             lambda history, new: history.at[self.step].set(jnp.max(new)), self.history, actual_static
         )
         self.step = (self.step + 1) % self.history_length
         static = jtu.tree_map(lambda x: int(jnp.max(x)), self.history)
         static = static.round_with_padding(self.padding_factor, self.n_electrons, self.n_up, self.n_nuclei)
+
+        # Check if the static input has changed (ie. probably triggering a recompile)
+        if any([x != y for x, y in zip(jtu.tree_leaves(static), jtu.tree_leaves(self.previous_static))]):
+            self.steps_since_recompile = 0
+            self.previous_static = static
+            self.padding_factor = self.max_padding_factor
+
+        # If the statics have not changed in a while, reduce the padding factor
+        if self.steps_since_recompile > 100:
+            self.padding_factor = self.min_padding_factor
         return static
