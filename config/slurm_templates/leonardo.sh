9dc76f69e5bacf086e49e476eecf39b569cf9215
Michael Scherbela <michael.scherbela@gmail.com>
2024-11-08 10:58:22 +0100

    Enable single process per node; switch leonardo default to single process per node

--- config/slurm_templates/leonardo.sh
+++ config/slurm_templates/leonardo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -J {job_name}
 #SBATCH -N {(n_gpus-1)//4 + 1}
+#SBATCH --gpus-per-task={min(n_gpus, 4)}
+#SBATCH --ntasks-per-node=1
 #SBATCH -p boost_usr_prod
 #SBATCH --qos normal
 #SBATCH -A L-AUT_Sch-Hoef
 #SBATCH --output stdout.txt
 #SBATCH --time {time}
-#SBATCH --gres=gpu:{min(n_gpus, 4)}
-#SBATCH --ntasks-per-node={min(n_gpus, 4)}
 #SBATCH --signal=B:USR1@300
 #SBATCH --export=NONE
 SLURM_EXPORT_ENV=ALL
