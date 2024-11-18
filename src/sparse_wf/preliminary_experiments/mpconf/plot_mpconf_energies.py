caf11ac42546b4ae61d03bd0cc5b73a1ce4c205b
Michael Scherbela <michael.scherbela@gmail.com>
2024-11-18 15:18:39 +0100

    Update mpconf plot

--- src/sparse_wf/preliminary_experiments/mpconf/plot_mpconf_energies.py
+++ src/sparse_wf/preliminary_experiments/mpconf/plot_mpconf_energies.py
@@ -5,15 +5,21 @@ import pandas as pd
 
 api = wandb.Api()
 runs = api.runs("tum_daml_nicholas/mpconf")
-runs = [r for r in runs if r.name.startswith("mpconf_WG") or r.name.startswith("mpconf_hf_v2_WG")]
+runs = [r for r in runs if r.name.startswith("mpconf_WG") or r.name.startswith("mpconf_hf_v2_WG") or r.name.startswith("mpconf_damp1e-3_WG")]
 
 data = []
 for r in runs:
     print(r.name)
+    if "hf_v2" in r.name:
+        model = "hf_like"
+    elif "mpconf_damp1e-3" in r.name:
+        model = "swann_high_damp"
+    else:
+        model = "swann_low_damp"
     metadata = dict(
         molecule=r.config["molecule_args"]["database_args"]["comment"],
         run_name=r.name,
-        model="hf_like" if "mpconf_hf" in r.name else "swann")
+        model=model)
 
     for h in r.scan_history(["opt/step", "opt/E", "opt/E_std"], page_size=10_000):
         data.append(h | metadata)
@@ -26,11 +32,12 @@ import bokeh.plotting as bpl
 import bokeh
 
 window_length = 5_000
+models = ["hf_like", "swann_low_damp", "swann_high_damp"]
 
 df_all = pd.read_csv("mpconf_energies.csv")
 molecules = sorted(df_all["molecule"].unique())
 pivot_all = df_all.pivot_table(index="opt/step", columns=["model", "molecule"], values="opt/E", aggfunc="mean")
-for model in ["hf_like", "swann"]:
+for model in models:
     E_smooth = 0.0
     molecules = list(pivot_all[model])
     for m in molecules:
@@ -43,7 +50,7 @@ for model in ["hf_like", "swann"]:
 
 
 plots = []
-for model, title in zip(["hf_like", "swann"], ["HF-like model", "Full SWANN"]):
+for model, title in zip(models, ["HF-like model (high damping)", "Full SWANN (low damping)", "Full SWANN (high damping)"]):
     pivot = pivot_all[model]
     molecules = [m for m in list(pivot) if m.startswith("WG_")]
 
@@ -52,15 +59,15 @@ for model, title in zip(["hf_like", "swann"], ["HF-like model", "Full SWANN"]):
     df_ref = df_ref.groupby("geom_comment")[["E"]].mean()
     df_ref["delta"] = (df_ref - df_ref.mean(axis=0)) * 1000
 
-    p = bpl.figure(width=600, height=600, title=f"Relative Energies vs CCSD(T): {title}", x_axis_label="Optimization Step", y_axis_label="E - E_mean / mHa", tools="box_zoom,hover,save", tooltips=[("step", "$x"), ("delta", "$y")])
+    p = bpl.figure(width=400, height=600, title=f"Relative Energies vs CCSD(T): {title}", x_axis_label="Optimization Step / k", y_axis_label="E - E_mean / mHa", tools="box_zoom,hover,save", tooltips=[("step", "$x"), ("delta", "$y")])
     for m, color in zip(molecules, bokeh.palettes.HighContrast3):
-        p.line(pivot.index, pivot[f"delta_{m}"], legend_label=m, color=color, line_width=2)
+        p.line(pivot.index / 1000, pivot[f"delta_{m}"], legend_label=m, color=color, line_width=2)
         p.add_layout(bokeh.models.Span(location=df_ref.loc[m, "delta"], dimension="width", line_color=color, line_dash="dashed", line_width=2))
     p.y_range.start = -10
     p.y_range.end = 10
     plots.append(p)
 
-grid = bokeh.layouts.gridplot(plots, ncols=2)
+grid = bokeh.layouts.gridplot(plots, ncols=3)
 bpl.output_notebook()
 bpl.show(grid)
 bokeh.io.export_png(grid, filename="mpconf.png")
