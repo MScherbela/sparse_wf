f30b869f9566a5252a0a29339d9a20ac7937115b
Michael Scherbela <michael.scherbela@gmail.com>
2025-02-05 14:36:48 +0100

    Pin jax to 0.4.29

--- src/sparse_wf/preliminary_experiments/benzene_dimer/benzene_dimer.py
+++ src/sparse_wf/preliminary_experiments/benzene_dimer/benzene_dimer.py
@@ -5,11 +5,7 @@ import re
 
 api = wandb.Api()
 all_runs = api.runs("tum_daml_nicholas/benzene")
-runs_cutoff3 = [r for r in all_runs if r.name.startswith("3.0_benzene_dimer_T_")]
-runs_cutoff5 = [r for r in all_runs if r.name.startswith("5.0_benzene_dimer_T_")]
-# runs_cutoff5 = [r for r in runs_cutoff5 if not "leonardo" in r.name]
-runs_cutoff5 = [r for r in runs_cutoff5 if "leonardo" in r.name]
-runs = runs_cutoff3 + runs_cutoff5
+runs = [r for r in all_runs if re.match("HLR_.*", r.name)]
 
 data = []
 for r in runs:
@@ -19,20 +15,18 @@ for r in runs:
     cutoff=5.0 if "5.0" in r.name else 3.0,
     run_name=r.name,)
 
-    for h in r.scan_history(["opt/step", "opt/E", "opt/E_std"], page_size=10_000):
+    for h in r.scan_history(["opt/step", "opt/E"], page_size=10_000):
         data.append(h | metadata)
 
 df_all = pd.DataFrame(data)
 df_all.to_csv("benzene_energies.csv", index=False)
 
 #%%
-import bokeh.layouts
-import bokeh.plotting as bpl
-import bokeh
+import matplotlib.pyplot as plt
 
-window_length = 10_000
-cutoffs = [3.0, 5.0]
-# cutoffs = [3.0]
+window_length = 1_000
+# cutoffs = [3.0, 5.0]
+cutoffs = [3.0]
 dists = [4.95, 10.0]
 
 df_all = pd.read_csv("benzene_energies.csv")
@@ -45,34 +39,47 @@ for cutoff in cutoffs:
     pivot.loc[:, (cutoff, "delta_smooth")] = (pivot[cutoff][f"E{dists[0]}_smooth"] - pivot[cutoff][f"E{dists[1]}_smooth"]) * 1000
 
 refs = {
-"Experiment": -3.8,
-"PsiFormer": 5.0,
-"FermiNet VMC (Glehn et al)": -4.6,
-"FermiNet DMC (Ren et al)": -9.2,
+"Experiment": (-3.8, "k"),
+"PsiFormer": (5.0, "forestgreen"),
+"FermiNet VMC (Glehn et al)": (-4.6, "C0"),
+"FermiNet DMC (Ren et al)": (-9.2, "navy")
 }
 
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+for ref, (E_ref, color) in refs.items():
+    ax.axhline(E_ref, color=color, linestyle="dashed")
+    ax.text(1, E_ref, ref, color=color, va="bottom", ha="left")
 
-# df_ref = pd.read_csv("../../../../data/energies.csv")
-# df_ref = df_ref[(df_ref.model == "CCSD(T)") & (df_ref.model_comment == "CBS") & (df_ref.geom_comment.isin(molecules))]
-# df_ref = df_ref.groupby("geom_comment")[["E"]].mean()
-# df_ref["delta"] = (df_ref - df_ref.mean(axis=0)) * 1000
+for cutoff in cutoffs:
+    ax.plot(pivot.index / 1000, pivot[(cutoff, "delta_smooth")], label=f"SWANN cutoff={cutoff:.1f}", color="red")
+ax.legend(loc="upper right")
+ax.set_ylim([-10, 6])
+ax.set_xlabel("Opt Step / k")
+ax.set_ylabel("E_4.95 - E_10.0 / mHa")
+ax.set_title("Benzene dimer binding energy")
+fig.savefig("benzene_dimer.png")
 
-p = bpl.figure(width=900, title="Benzene dimer binding energy", x_axis_label="Opt Step / k", y_axis_label="E_4.95 - E_10.0 / mHa", tools="box_zoom,hover,save", tooltips=[("step", "$x"), ("delta", "$y")])
-for cutoff, color in zip(cutoffs, bokeh.palettes.HighContrast3):
-    p.line(pivot.index / 1000, pivot[(cutoff, "delta_smooth")], legend_label=f"SWANN cutoff={cutoff:.1f}", color=color, line_width=2)
-    # p.add_layout(bokeh.models.Span(location=df_ref.loc[m, "delta"], dimension="width", line_color=color, line_dash="dashed", line_width=2))
+# # df_ref = pd.read_csv("../../../../data/energies.csv")
+# # df_ref = df_ref[(df_ref.model == "CCSD(T)") & (df_ref.model_comment == "CBS") & (df_ref.geom_comment.isin(molecules))]
+# # df_ref = df_ref.groupby("geom_comment")[["E"]].mean()
+# # df_ref["delta"] = (df_ref - df_ref.mean(axis=0)) * 1000
 
-for (ref, E_ref), color in zip(refs.items(), ("black",) + bokeh.palettes.Category10[10]):
-    line_dash = "solid" if ref == "Experiment" else "dashed"
-    p.line([0, max(pivot.index / 1000)], [E_ref, E_ref], legend_label=ref, color=color, line_dash=line_dash, line_width=2)
+# p = bpl.figure(width=900, title="Benzene dimer binding energy", x_axis_label="Opt Step / k", y_axis_label="E_4.95 - E_10.0 / mHa", tools="box_zoom,hover,save", tooltips=[("step", "$x"), ("delta", "$y")])
+# for cutoff, color in zip(cutoffs, bokeh.palettes.HighContrast3):
+#     p.line(pivot.index / 1000, pivot[(cutoff, "delta_smooth")], legend_label=f"SWANN cutoff={cutoff:.1f}", color=color, line_width=2)
+#     # p.add_layout(bokeh.models.Span(location=df_ref.loc[m, "delta"], dimension="width", line_color=color, line_dash="dashed", line_width=2))
+
+# for (ref, E_ref), color in zip(refs.items(), ("black",) + bokeh.palettes.Category10[10]):
+#     line_dash = "solid" if ref == "Experiment" else "dashed"
+#     p.line([0, max(pivot.index / 1000)], [E_ref, E_ref], legend_label=ref, color=color, line_dash=line_dash, line_width=2)
 
 
-p.y_range.start = -10
-p.y_range.end = 10
-bpl.output_notebook()
-bpl.show(p)
+# p.y_range.start = -10
+# p.y_range.end = 10
+# bpl.output_notebook()
+# bpl.show(p)
 
-bokeh.io.export_png(p, filename="benzene_dimer.png")
+# bokeh.io.export_png(p, filename="benzene_dimer.png")
 
 
 
