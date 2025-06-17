dea5cda0f711a63ca6a0cbb375c1c89809e2ba0e
Michael Scherbela <michael.scherbela@gmail.com>
2025-06-17 12:12:10 +0200

    Remove preliminary experiments directory

--- src/sparse_wf/preliminary_experiments/parallelization/distributed/test_distributed.py
+++
@@ -1,9 +0,0 @@
-import jax.distributed
-import jax
-
-if __name__ == "__main__":
-    jax.distributed.initialize()
-    print(
-        f"Process {jax.process_index()}: Global devices: {jax.devices()}, Local devices: {jax.local_devices()}",
-        flush=True,
-    )
