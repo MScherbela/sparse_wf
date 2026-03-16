fafff90030fd114a6447d7458b83ad62d24bb92c
Michael Scherbela <michael.scherbela@gmail.com>
2026-03-16 13:30:27 +0100

    Plot for ccsdt vs fire

--- src/sparse_wf/plot_scripts/orca_runtime/plot_orca_runtime.py
+++ src/sparse_wf/plot_scripts/orca_runtime/plot_orca_runtime.py
@@ -1,6 +1,6 @@
 # %%
 import matplotlib.pyplot as plt
-from sparse_wf.plot_utils import COLOR_PALETTE
+from sparse_wf.plot_utils import COLOR_PALETTE, COLOR_FIRE
 import pandas as pd
 import seaborn as sns
 import numpy as np
@@ -9,6 +9,10 @@ sns.set_style("whitegrid")
 
 N_EL_MIN_FOR_FIT = 28
 BASIS_SET = "cc-pVTZ"
+FIRE_BATCH_SIZE = 512  # 1 node with 8 GPUs for 4096 total batch size
+NUM_STEPS_BASE_CASE = 100e3
+NUM_ELEC_BASE_CASE = 68
+NUM_STEPS_SCALING_EXPONENT = 2.3
 
 
 def fit_and_plot(ax, x, y, color, ls="-", n_fit_min=N_EL_MIN_FOR_FIT):
@@ -27,80 +31,57 @@ def format_exponent(exp):
     return f"$O(n^{{{exp:.1f}}})$"
 
 
-df = pd.read_csv("cumulene_orca.csv")
-df = df.sort_values(["num_atoms", "num_basis_functions"])
-df["num_valence_electrons"] = (df.num_atoms - 4) * 4 + 4
+# CCSD(T)
+df_ccsdt = pd.read_csv("cumulene_orca.csv")
+df_ccsdt = df_ccsdt.sort_values(["num_atoms", "num_basis_functions"])
+df_ccsdt["num_valence_electrons"] = (df_ccsdt.num_atoms - 4) * 4 + 4
+
+
+# FiRE
+df_fire = pd.read_csv("../scaling/data/full_run_data.csv")
+df_fire = df_fire[df_fire.model == "FiRE"]
+df_fire = df_fire[~df_fire["opt/t_step"].isnull()]
+df_fire["t_step"] = df_fire["opt/t_step"] * FIRE_BATCH_SIZE / df_fire.batch_size
+df_fire = df_fire.groupby(["n_carbon", "cutoff"])["t_step"].min().reset_index()
+df_fire["num_valence_electrons"] = df_fire.n_carbon * 4 + 4
+df_fire["num_steps"] = (
+    NUM_STEPS_BASE_CASE * (df_fire.num_valence_electrons / NUM_ELEC_BASE_CASE) ** NUM_STEPS_SCALING_EXPONENT
+)
+df_fire["total_hours"] = df_fire.t_step * df_fire.num_steps / 3600
+
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 5))
-df_basis = df[(df.basis_set == BASIS_SET) & df.success]
+df_basis = df_ccsdt[(df_ccsdt.basis_set == BASIS_SET) & df_ccsdt.success]
 
-color = "C0"
-ax.scatter(df_basis.num_valence_electrons, df_basis.total_hours, color=color)
+color = COLOR_PALETTE[0]
 exp = fit_and_plot(ax, df_basis.num_valence_electrons, df_basis.total_hours, color=color)
 
 label = f"CCSD(T)/{BASIS_SET}: {format_exponent(exp)}"
+ax.loglog(df_basis.num_valence_electrons, df_basis.total_hours, color=color, marker="o", ls="none", label=label)
+
+
+cutoff = 3.0
+df_cutoff = df_fire[df_fire.cutoff == cutoff]
+color = COLOR_FIRE
+ax.scatter(df_cutoff.num_valence_electrons, df_cutoff.total_hours, color=color)
+exp = fit_and_plot(ax, df_cutoff.num_valence_electrons, df_cutoff.total_hours, color=color)
+
+label = f"FiRE (c={cutoff:.1f}$a_0$): {format_exponent(exp)}"
 ax.loglog(
-    df_basis.num_valence_electrons,
-    df_basis.total_hours,
+    df_cutoff.num_valence_electrons,
+    df_cutoff.total_hours,
     color=color,
-    marker="o",
+    marker="s",
     ls="none",
     label=label,
 )
 
-
-df_fire = pd.read_csv("../scaling/data/timings_4k.csv")
-
-
 X_TICKS = [20, 30, 50, 100, 140, 200, 300, 500]
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xticks(X_TICKS)
 ax.set_xticklabels([str(x) for x in X_TICKS])
 ax.legend()
-
-
-# %%
-
-fig, axes = plt.subplots(1, 2, figsize=(8, 5))
-BASIS_SETS = ["cc-pVDZ", "cc-pVTZ"]  # , "cc-pVQZ"]
-X_TICKS = [20, 30, 50, 100, 140, 200, 300, 500]
-
-ax = axes[0]
-for idx_basis, basis_set in enumerate(BASIS_SETS):
-    color = COLOR_PALETTE[idx_basis]
-    df_basis = df[df.success & (df.basis_set == basis_set)]
-    exp = fit_and_plot(
-        ax,
-        df_basis.num_valence_electrons,
-        df_basis.total_hours,
-        color,
-    )
-    label = f"{basis_set}: {format_exponent(exp)}"
-    ax.loglog(
-        df_basis.num_valence_electrons,
-        df_basis.total_hours,
-        color=color,
-        marker="o",
-        ls="none",
-        label=label,
-    )
-
-ax.set_xticks(X_TICKS)
-ax.set_xticklabels([str(x) for x in X_TICKS])
-ax.legend()
-
-ax = axes[1]
-sns.scatterplot(
-    df,
-    x="num_basis_functions",
-    # y="ccsd_step_seconds",
-    y="total_hours",
-    hue="basis_set",
-    ax=ax,
-)
-ax.set_xscale("log")
-ax.set_yscale("log")
 
 
 # %%
